c6ca426bb44ff054b1e9e9a4e76960508c776c3c
stevehndz <steve.r.hndz@gmail.com>
2024-06-24 15:50:51 -0600

    feat: image slider

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { FaMap } from "react-icons/fa";
+
+interface CampProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
+
+const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampProps) => {
+  return (
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl xl:rounded-5xl`}
+    >
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
+        <div className="flexCenter gap-4">
+          <div className="rounded-full bg-green-50 p-4">
+            <FaMap className="w-6 h-6 text-white" />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h4 className="bold-18 text-white">{title}</h4>
+            <p className="regular-14 text-white">{subtitle}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Camp = () => {
   return (
-    <div>Camp</div>
-  )
-}
+    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CampSite
+          backgroundImage="bg-bg-img-1"
+          title="Cerro Verde"
+          subtitle="Apaneca, Santa Ana"
+          peopleJoined="50+ Joined"
+        />
+        <CampSite
+          backgroundImage="bg-bg-img-2"
+          title="Cerro Verde"
+          subtitle="Apaneca, Santa Ana"
+          peopleJoined="50+ Joined"
+        />
+      </div>
+    </section>
+  );
+};
 
-export default Camp
+export default Camp;
